c01df2c47467813f010e5644ac3c062fc3afe997
Artur Shamsutdinov <artur.shamsutdinov@gmail.com>
2015-05-04 23:49:18 +1000

    init

---
+++ Additions/MapView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Org.Osmdroid.Views
+{
+	public partial class MapView : Org.Osmdroid.Api.IMapView
+	{
+		public void SetBackgroundColor(int color){
+		}
+	}
+}
+
---
+++ Additions/MyLocationOverlay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Org.Osmdroid.Views.Overlay
+{
+	public partial class MyLocationOverlay
+	{
+		public void OnStatusChanged(string p1, int p2, Android.OS.Bundle p3) {
+		}
+	}
+}
+
